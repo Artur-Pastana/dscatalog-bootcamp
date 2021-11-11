7d18bf8a6ada876ee77187ce4e97dc165889f489
Artur-Pastana <reitutu_15@hotmail.com>
2021-11-11 00:17:36 -0300

    Database seedind

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Limpeza');
+INSERT INTO tb_category (name) VALUES ('Armarinho');
+INSERT INTO tb_category (name) VALUES ('Papelaria');
+INSERT INTO tb_category (name) VALUES ('Musculação');
