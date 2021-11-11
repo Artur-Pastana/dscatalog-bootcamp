b0c375323930338bf54ce13847181bb9b107f817
Artur-Pastana <reitutu_15@hotmail.com>
2021-11-11 12:41:41 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/arturdev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/arturdev/dscatalog/services/CategoryService.java
@@ -1,13 +1,17 @@
 package com.arturdev.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.arturdev.dscatalog.dto.CategoryDTO;
 import com.arturdev.dscatalog.entities.Category;
 import com.arturdev.dscatalog.repositories.CategoryRepository;
+import com.arturdev.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,7 +20,15 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		return new CategoryDTO(entity);
 	}
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,3 +3,5 @@ INSERT INTO tb_category (name) VALUES ('Limpeza');
 INSERT INTO tb_category (name) VALUES ('Armarinho');
 INSERT INTO tb_category (name) VALUES ('Papelaria');
 INSERT INTO tb_category (name) VALUES ('Musculação');
+INSERT INTO tb_category (name) VALUES ('natação');
+INSERT INTO tb_category (name) VALUES ('arte marcial');
